28597aa4d09440502a47a2cd65d99ba11c2e64db
Philip Helger <philip@helger.com>
2020-05-19 20:57:06 +0200

    Made Http Client Settings customizable

--- dsd-client/src/main/java/eu/toop/dsd/client/DSDClient.java
+++ dsd-client/src/main/java/eu/toop/dsd/client/DSDClient.java
@@ -1,21 +1,24 @@
 package eu.toop.dsd.client;
 
-import com.helger.commons.ValueEnforcer;
-import com.helger.commons.url.SimpleURL;
-import com.helger.pd.searchapi.v1.MatchType;
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import java.io.ByteArrayOutputStream;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
+
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.helger.commons.ValueEnforcer;
+import com.helger.commons.annotation.Nonempty;
+import com.helger.commons.io.stream.NonBlockingByteArrayOutputStream;
+import com.helger.commons.url.SimpleURL;
+import com.helger.httpclient.HttpClientManager;
+import com.helger.httpclient.HttpClientSettings;
+import com.helger.pd.searchapi.v1.MatchType;
 
 public class DSDClient {
 
@@ -26,20 +29,40 @@ public class DSDClient {
   public static final String PARAM_NAME_QUERY_ID = "queryId";
   public static final String PARAM_NAME_COUNTRY_CODE = "countryCode";
 
+  private final String m_sDSDBaseURL;
+  private HttpClientSettings m_aHttpClientSettings;
+
   /**
-   * The default DSD query as described here: http://wiki.ds.unipi.gr/display/TOOPSA20/Data+Services+Directory
+   * Constructor
    *
-   * @param dsdBaseUrl  the URL where the DSD service resides, may not be null
-   * @param datasetType the dataset type, may not be null
+   * @param dsdBaseUrl the URL where the DSD service resides, may not be
+   *                   <code>null</code>
+   */
+  public DSDClient(@Nonnull @Nonempty final String dsdBaseUrl) {
+    ValueEnforcer.notEmpty(dsdBaseUrl, "DSD BaseURL");
+    m_sDSDBaseURL = dsdBaseUrl;
+  }
+
+  @Nonnull
+  public DSDClient setHttpClientSettings(@Nullable final HttpClientSettings aHttpClientSettings) {
+    m_aHttpClientSettings = aHttpClientSettings;
+    return this;
+  }
+
+  /**
+   * The default DSD query as described here:
+   * http://wiki.ds.unipi.gr/display/TOOPSA20/Data+Services+Directory
+   *
+   * @param datasetType the dataset type, may not be <code>null</code>
    * @param countryCode the country code, optional
    * @return the list of {@link MatchType} objects.
    */
-  public static List<MatchType> queryDataset(@Nonnull String dsdBaseUrl, @Nonnull String datasetType,
-                                      @Nullable String countryCode) {
+  @Nullable
+  public List<MatchType> queryDataset(@Nonnull final String datasetType, @Nullable final String countryCode) {
 
     ValueEnforcer.notEmpty(datasetType, "datasetType");
 
-    final SimpleURL aBaseURL = new SimpleURL(dsdBaseUrl + "/rest/search");
+    final SimpleURL aBaseURL = new SimpleURL(m_sDSDBaseURL + "/rest/search");
 
     aBaseURL.add(PARAM_NAME_QUERY_ID, QUERY_DATASET_REQUEST);
     aBaseURL.add(PARAM_NAME_DATA_SET_TYPE, datasetType);
@@ -52,26 +75,29 @@ public class DSDClient {
     if (LOGGER.isInfoEnabled())
       LOGGER.info("Querying " + aBaseURL.getAsStringWithEncodedParameters());
 
-    try (final CloseableHttpClient httpClient = HttpClients.createDefault()) {
+    final HttpClientSettings aHttpClientSettings = m_aHttpClientSettings != null ? m_aHttpClientSettings
+                                                                                 : new HttpClientSettings();
+    try (final HttpClientManager httpClient = HttpClientManager.create(aHttpClientSettings)) {
       final HttpGet aGet = new HttpGet(aBaseURL.getAsURI());
 
-      final HttpResponse response = httpClient.execute(aGet);
+      try (final CloseableHttpResponse response = httpClient.execute(aGet)) {
+        if (response.getStatusLine().getStatusCode() != HttpStatus.SC_OK) {
+          throw new IllegalStateException("Request failed " + response.getStatusLine().getStatusCode());
+        }
 
-      if (response.getStatusLine().getStatusCode() != HttpStatus.SC_OK) {
-        throw new IllegalStateException("Request failed " + response.getStatusLine().getStatusCode());
+        try (final NonBlockingByteArrayOutputStream stream = new NonBlockingByteArrayOutputStream()) {
+          response.getEntity().writeTo(stream);
+          final byte[] s_bytes = stream.toByteArray();
+          if (LOGGER.isDebugEnabled()) {
+            final String s_result = new String(s_bytes, StandardCharsets.UTF_8);
+            LOGGER.debug("DSD result:\n" + s_result);
+          }
+          return DsdResponseReader.matchTypeListReader().read(s_bytes);
+        }
       }
-
-      try (final ByteArrayOutputStream stream = new ByteArrayOutputStream()) {
-        response.getEntity().writeTo(stream);
-        final byte[] s_bytes = stream.toByteArray();
-
-        final String s_result = new String(s_bytes, StandardCharsets.UTF_8);
-        LOGGER.debug(s_result);
-        return DsdResponseReader.matchTypeListReader().read(s_result);
-      }
-    } catch (RuntimeException ex) {
+    } catch (final RuntimeException ex) {
       throw ex;
-    } catch (Exception ex) {
+    } catch (final Exception ex) {
       LOGGER.error(ex.getMessage(), ex);
       throw new IllegalStateException(ex);
     }
--- dsd-client/src/test/java/eu/toop/dsd/service/DSDClientTest.java
+++ dsd-client/src/test/java/eu/toop/dsd/service/DSDClientTest.java
@@ -1,48 +1,41 @@
 package eu.toop.dsd.service;
 
-
 import java.io.StringWriter;
+import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.List;
 
 import javax.xml.datatype.DatatypeConfigurationException;
 import javax.xml.datatype.DatatypeFactory;
 
-import eu.toop.dsd.client.DSDClient;
-import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 
-import com.helger.pd.searchapi.PDSearchAPIReader;
 import com.helger.pd.searchapi.PDSearchAPIWriter;
 import com.helger.pd.searchapi.v1.MatchType;
 import com.helger.pd.searchapi.v1.ResultListType;
 
-import eu.toop.dsd.client.DsdResponseReader;
-import eu.toop.dsd.client.DsdResponseWriter;
-import eu.toop.dsd.client.types.DoctypeParts;
+import eu.toop.dsd.client.DSDClient;
 
-public class DSDClientTest {
+public final class DSDClientTest {
 
   @Ignore
   @Test
   public void testQuery() throws DatatypeConfigurationException {
-    List<MatchType> matchTypes = DSDClient.queryDataset("http://dsd.dev.exchange.toop.eu", "REGISTERED_ORGANIZATION_TYPE", "SV");
+    final List<MatchType> matchTypes = new DSDClient("http://dsd.dev.exchange.toop.eu").queryDataset("REGISTERED_ORGANIZATION_TYPE",
+                                                                                                     "SV");
 
-    ResultListType rls = new ResultListType();
+    final ResultListType rls = new ResultListType();
     rls.setVersion("1");
     rls.setQueryTerms("terms");
-    GregorianCalendar c = (GregorianCalendar) GregorianCalendar.getInstance();
+    final GregorianCalendar c = (GregorianCalendar) Calendar.getInstance();
     rls.setCreationDt(DatatypeFactory.newInstance().newXMLGregorianCalendar(c));
     matchTypes.forEach(matchType -> {
       rls.addMatch(matchType);
     });
 
-
-    StringWriter writer = new StringWriter();
+    final StringWriter writer = new StringWriter();
     PDSearchAPIWriter.resultListV1().write(rls, writer);
     System.out.println(writer);
   }
 }
-
-
